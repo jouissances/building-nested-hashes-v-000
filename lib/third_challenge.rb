11628b24957f1c34877e6ecd29c99775f8580a34
Janice Darikho <janicedarikho@gmail.com>
2018-05-22 10:15:12 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,17 +6,22 @@ def third_challenge
           :age => "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        { :name => 'Lady Montague',
+          :age => "54"
         },
       :hero => {
-        #your key/value pairs here
-        },
+        { :name => 'Romeo',
+          :age => "15",
+          :status => "Alive"
+        },        
+      },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
-        },
+        { :name => 'Lord Capulet',
+          :age => "50"
+        },        },
       :matriarch => {
         #your key/value pairs here
         },
