f51d1e752ddca36d954c74113ffd693a81ffd674
Janice Darikho <janicedarikho@gmail.com>
2018-05-22 10:21:38 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,31 +1,17 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      :patriarch => {},
-      :matriarch => {}.
-      :hero => {},
-      :hero_friends => {[]}
-      }, 
-    :capulet => {
+   :montague => {
       :patriarch => {},
       :matriarch => {},
-      :heroine => {},
-      :heroine_Friends => {[]}
-      }}
-end
-
-epic_tragedy = {
-   :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero => {},
       :hero_friends => []
    },
    :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
       :heroine_friends => []
    }
-}
- 
+  }
+end
+
